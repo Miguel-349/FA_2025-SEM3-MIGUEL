331edd1dde08e80fbd64b61ba08def454267b69f
miguel <migiigf@gmail.com>
2025-09-03 12:06:01 -0600

    subiendo ejercicio 1

---
+++ SEMAN1_C#/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEMAN1_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EJER1();
+            Console.ReadKey();
+        }
+
+        static void EJER1()
+        {
+            string nombre, carrera;
+
+            Console.Write("Ingrese su nombre: ");
+            nombre = Console.ReadLine();
+            Console.Write("Ingresar su carrera: ");
+            carrera = Console.ReadLine();
+
+            Console.WriteLine($"\n{nombre}, bienvenido al curso de FA de la carrera {carrera}");
+        }
+        static void EJER2()
+        {
+            EJER3();
+            
+        }
+
+        static void EJER3()
+        {
+            
+           
+        }
+        static void EJER4()
+        {
+           EJER5();
+         
+
+        }
+        static void EJER5()
+        {
+            EJER6();
+            
+
+        }
+        static void EJER6()
+        {
+            
+
+        }
+
+
+    }
+}
